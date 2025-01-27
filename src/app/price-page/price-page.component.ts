50efaf4b0d2f7738b15932820f5ea95916a3a3a8
Alice Lucena <alicetluc@gmail.com>
2025-01-27 15:48:25 -0400

    create price-page component

---
+++ src/app/price-page/price-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule } from "@angular/core";
+
+@Component({
+  selector: "app-price-page",
+  templateUrl: "./price-page.component.html",
+  styleUrls: ["./price-page.component.scss"]
+})
+export class PricePageComponent {
+}
+
+@NgModule({
+  declarations: [PricePageComponent],
+  exports: [PricePageComponent],
+  imports: []
+})
+export class PricePageModule {}
